d545f12d19e4bd24514e6e9b379bb7dc47233b19
MatheusFC2 <matheusfreitascampos@gmail.com>
2024-01-21 19:21:16 -0300

    feat: create categories useCases

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import { router } from "./router";
 
 const app = express();
 
@@ -7,6 +8,10 @@ mongoose
 	.connect("mongodb://localhost:27017")
 	.then(() => {
 		const port = 3001;
+
+		app.use(express.json());
+		app.use(router);
+
 		app.listen(port, () => {
 			console.log(`listening on http://localhost:${port}`);
 		});
