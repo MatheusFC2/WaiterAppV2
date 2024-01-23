3ac7ebdb89086799db6ee9c497c0c83577bc87ed
MatheusFC2 <matheusfreitascampos@gmail.com>
2024-01-23 12:19:00 -0300

    feat: name and icon validation

--- api/src/app/useCases/categories/createCategory.ts
+++ api/src/app/useCases/categories/createCategory.ts
@@ -2,9 +2,22 @@ import { Request, Response } from "express";
 import { Category } from "../../models/Category";
 
 export async function createCategory(req: Request, res: Response) {
-	const { icon, name } = req.body;
+	try {
+		const { icon, name } = req.body;
 
-	const category = await Category.create({ icon, name });
+		if (!name) {
+			return res.status(400).json({ message: "Name is required" });
+		}
 
-	res.json(category);
+		if (!icon) {
+			return res.status(400).json({ message: "Icon is required" });
+		}
+
+		const category = await Category.create({ icon, name });
+
+		res.status(201).json(category); // if result is 201, return the category.
+	} catch (error) {
+		console.log(error);
+		res.status(500);
+	}
 }
