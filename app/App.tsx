5309f5f4c7b913be3c9595ab9c51e740feaf44cf
MatheusFC2 <matheusfreitascampos@gmail.com>
2024-03-12 19:55:39 -0300

    feat: set setup config app

---
+++ app/App.tsx
@@ -0,0 +1,27 @@
+import { useFonts } from "expo-font";
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+    useFonts({
+        "GeneralSans-400": require("./src/assets/fonts/GeneralSans-Regular.otf"),
+        "GeneralSans-500": require("./src/assets/fonts/GeneralSans-Semibold.otf"),
+        "GeneralSans-600": require("./src/assets/fonts/GeneralSans-Bold.otf"),
+    });
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#d4d4d4",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
